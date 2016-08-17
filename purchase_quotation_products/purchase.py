2dabeb846c11c02307322bab6f52c1a24429de6d
susport <richard.pompa84@gmail.com>
2016-08-17 10:11:19 -0300

    9.0-mig-purchase_quotation_products
    Usando onchange_product_id

--- purchase_quotation_products/purchase.py
+++ purchase_quotation_products/purchase.py
@@ -43,64 +43,22 @@ class purchase_order(models.Model):
         self.ensure_one()
         for product in self.env['product.product'].browse(
                 product_ids):
-            line_data = self.env['purchase.order.line'].\
-                onchange_product_quotation(product, self)
+
+            product_lang = product.with_context({
+                'lang': self.partner_id.lang,
+                'partner_id': self.partner_id.id})
+
             val = {
-                'name': line_data['name'],
-                'date_planned': line_data['date_planned'],
+                'name': product_lang.display_name,
+                'date_planned': datetime.today().strftime(
+                    DEFAULT_SERVER_DATETIME_FORMAT),
                 'product_qty': qty,
                 'order_id': self.id,
                 'product_id': product.id or False,
-                'product_uom': line_data['product_uom'].id,
-                'price_unit': line_data['price_unit'],
-                'taxes_id': [(6, 0, line_data['taxes_id'])],
+                'product_uom': product.uom_id.id,
+                'price_unit': 0.0,
             }
 
-            self.env['purchase.order.line'].create(val)
+            order_line = self.env['purchase.order.line'].create(val)
+            order_line.onchange_product_id()
 
-
-class purchase_order_line(models.Model):
-    _inherit = "purchase.order.line"
-
-    def onchange_product_quotation(
-            self,
-            product_id,
-            order_id):
-        result = {}
-        if not product_id:
-            return result
-
-        # Reset date, price and quantity since
-        #  _onchange_quantity will provide default values
-        result['date_planned'] = \
-            datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        result['price_unit'] = 0.0
-        result['product_uom'] = \
-            product_id.uom_po_id or product_id.uom_id
-        result['domain'] = \
-            {'product_uom': [('category_id',
-                              '=',
-                              product_id.uom_id.category_id.id)]}
-
-        product_lang = product_id.with_context({
-            'lang': self.partner_id.lang,
-            'partner_id': self.partner_id.id,
-        })
-        result['name'] = product_lang.display_name
-        if product_lang.description_purchase:
-            result['name'] += '\n' + product_lang.description_purchase
-
-        fpos = order_id.fiscal_position_id
-        if self.env.uid == SUPERUSER_ID:
-            company_id = self.env.user.company_id.id
-            result['taxes_id'] = fpos.map_tax(
-                product_id.supplier_taxes_id.filtered(
-                    lambda r: r.company_id.id == company_id))
-        else:
-            result['taxes_id'] = fpos.map_tax(
-                product_id.supplier_taxes_id)
-
-        self._suggest_quantity()
-        self._onchange_quantity()
-
-        return result
