e42b4b0ef0331a529a0752b180b44c3618dc6c7a
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2017-09-22 10:07:04 -0300

    [9.0] ADD purchase_global_discount (#16)
    * [9.0] ADD purchase_global_discount
    
    * FIX depends

---
+++ purchase_global_discount/wizard/purchase_global_discount_wizard.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class purchaseGlobalDiscountWizard(models.TransientModel):
+    _name = "purchase.order.global_discount.wizard"
+
+    # todo implement fixed amount
+    # type = fields.Selection([
+    #     ('percentage', 'Percentage'),
+    #     ('fixed_amount', 'Fixed Amount'),
+    #     ],
+    #     'Type',
+    #     required=True,
+    #     default='percentage',
+    #     )
+    amount = fields.Float(
+        # 'Amount',
+        'Discount',
+        required=True,
+    )
+
+    @api.multi
+    def confirm(self):
+        self.ensure_one()
+        order = self.env['purchase.order'].browse(
+            self._context.get('active_id', False))
+        for line in order.order_line:
+            line.discount = self.amount
+        return True
