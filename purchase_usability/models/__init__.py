db0752d067148584e3552d842aaca5804bc37e4f
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-02-02 09:05:02 -0300

    9.0 IMP purchase usability to use standard_price when use the planner to create PO (#21)

--- purchase_usability/models/__init__.py
+++ purchase_usability/models/__init__.py
@@ -7,3 +7,4 @@ from . import account_invoice
 from . import purchase_order
 from . import purchase_order_line
 from . import stock_move
+from . import procurement_order
---
+++ purchase_usability/models/procurement_order.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, api
+
+
+class ProcurementOrder(models.Model):
+    _inherit = 'procurement.order'
+
+    @api.multi
+    def _prepare_purchase_order_line(self, po, supplier):
+        self.ensure_one()
+        res = super(ProcurementOrder, self)._prepare_purchase_order_line(
+            po=po, supplier=supplier)
+        # if price was not computed (not seller or seller price = 0.0), then
+        # use standar price
+        if not res['price_unit']:
+            price_unit = self.product_id.standard_price
+            if (
+                    price_unit and
+                    po.currency_id != self.product_id.currency_id):
+                price_unit = self.product_id.currency_id.compute(
+                    price_unit, po.currency_id)
+            if (
+                    price_unit and res['product_uom'] and
+                    self.product_id.uom_id.id != res['product_uom']):
+                price_unit = self.env['product.uom']._compute_price(
+                    self.product_id.uom_id.id, price_unit,
+                    to_uom_id=res['product_uom'])
+            res['price_unit'] = price_unit
+        return res
