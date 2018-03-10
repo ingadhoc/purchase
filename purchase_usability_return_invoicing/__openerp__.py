b3872cf4ea7f40f1bf4d5adf93f51aefad781f42
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-03-10 12:22:28 -0300

    IMP filters related to invoicing states

--- purchase_usability_return_invoicing/__openerp__.py
+++ purchase_usability_return_invoicing/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Purchase Usability interatction with Refund management',
-    'version': '9.0.1.1.0',
+    'version': '9.0.1.2.0',
     'category': 'Purchases',
     'sequence': 14,
     'summary': '',
