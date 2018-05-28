6b4708ebdeddba44f6bc81d7d5b921ae5a92aa16
Valentino <vd@adhoc.com.ar>
2018-05-28 13:20:47 -0300

    [REL] purchase_global_discount to V11. [FIX] Update README [FIX] Update view file name

--- purchase_global_discount/__manifest__.py
+++ purchase_global_discount/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Purchase Global Discount',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Purchase Management',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -33,11 +33,9 @@
     },
     'data': [
         'wizard/purchase_global_discount_wizard_view.xml',
-        'views/purchase_order_view.xml',
+        'views/purchase_order_views.xml',
     ],
-    'demo': [
-    ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
