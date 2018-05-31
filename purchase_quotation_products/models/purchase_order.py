0adeda170c91b055afc63e1ce58fcc95c71651ad
nicomacr <nmr@adhoc.com.ar>
2018-05-31 11:36:30 -0300

    [FIX] some code fixes

--- purchase_quotation_products/models/purchase_order.py
+++ purchase_quotation_products/models/purchase_order.py
@@ -11,44 +11,52 @@ class PurchaseOrder(models.Model):
 
     @api.multi
     def add_products_to_quotation(self):
+        """ In order to filter the products of the partner the "product supplier
+        search" module need to be installed
+        """
         self.ensure_one()
         action_read = False
         actions = self.env.ref(
             'product.product_normal_action_sell')
+        view_id = self.env['ir.model.data'].xmlid_to_res_id(
+            'purchase_quotation_products.product_product_tree_view')
         if actions:
             action_read = actions.read()[0]
             context = literal_eval(action_read['context'])
             if 'search_default_filter_to_sell' in context:
                 context.pop('search_default_filter_to_sell')
-            context['search_default_filter_to_purchase'] = True
-            # para que este funcione requiere modulo product_supplier_search
-            context['search_default_seller_ids'] = self.partner_id.name
-            # {"search_default_filter_to_sell":1}
-            context['purchase_quotation_products'] = True
-            # context['pricelist'] = self.pricelist_id.display_name
-            # we send company in context so it filters taxes
-            context['company_id'] = self.company_id.id
-            context['partner_id'] = self.partner_id.id
-            action_read['context'] = context
-            # action_read['view_mode'] = 'tree,form'
-            action_read['name'] = _('Quotation Products')
+            context.update(dict(
+                search_default_filter_to_purchase=True,
+                search_default_seller_ids=self.partner_id.name,
+                purchase_quotation_products=True,
+                # we send company in context so it filters taxes
+                company_id=self.company_id.id,
+                # pricelist=self.pricelist_id.display_name,
+                partner_id=self.partner_id.id,
+            ))
+            action_read.update(dict(
+                context=context,
+                name=_('Quotation Products'),
+                views=[[view_id, 'tree']],
+                display_name=_('Quotation Products'),
+            ))
+        print ('action_read', action_read)
         return action_read
 
     @api.multi
     def add_products(self, product, qty):
+        """This method create line in cache to prepare the order line that
+        it's added to purchase order
+        """
         self.ensure_one()
         vals = {
             'order_id': self.id,
             'product_id': product.id or False,
         }
-        # we create line in cache
         purchase_line = self.env['purchase.order.line'].new(vals)
-        # we call onchange product to get required fields
         purchase_line.onchange_product_id()
         # we set qty (if we set it on create odoo overwrite it to 1)
         purchase_line.product_qty = qty
-        # we call onchange qty)
         purchase_line._onchange_quantity()
-        # we convert to write
         vals = purchase_line._convert_to_write(purchase_line._cache)
         self.env['purchase.order.line'].create(vals)
