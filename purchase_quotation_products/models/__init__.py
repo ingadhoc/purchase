570fdf536045679c8dfc9a489452ef124690be4c
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-06-30 17:10:38 -0300

    [REL] Migration purchase_quotation_products to 11.0 (#31)
    * [REL] Migration purchase_quotation_products to 11.0
    * [FIX] Update Readme
    * [FIX] change the xml files name
    * [FIX] rename the .py module files
    * [FIX] remove api.one decorator
    
    * [IMP] fields_view_get now it's in xml primary view
    
    * [FIX] Update Readme
    
    * [FIX] some code fixes
    
    * [FIX] change the way to inherit the fields in views
    
    * [FIX] some fixes
    
    * [FIX] remove print tag
    * [FIX] some fixes in views
    * [FIX] add 1 to run travis lint tets
    
    * [FIX] change inherit method contains with a simple name inherit
    
    * [IMP] use of xpath

--- purchase_quotation_products/models/__init__.py
+++ purchase_quotation_products/models/__init__.py
@@ -2,4 +2,5 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import models
+from . import product_product
+from . import purchase_order
---
+++ purchase_quotation_products/models/product_product.py
@@ -0,0 +1,71 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields, api, _
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    qty_purchase = fields.Float(
+        string='Quantity',
+        compute='_compute_qty_purchase',
+        inverse='_inverse_qty_purchase',
+        help="Technical field. Used to compute the quantity of products"
+        " related to a purchase order using the context",
+    )
+
+    @api.multi
+    def _compute_qty_purchase(self):
+        purchase_order_id = self._context.get('active_id', False)
+        ProductUom = self.env['product.uom']
+        if not purchase_order_id:
+            self.update({'qty_purchase': 0})
+            return
+
+        purchase_order_lines = self.env['purchase.order'].browse(
+            purchase_order_id).order_line
+
+        for rec in self:
+            lines = purchase_order_lines.filtered(
+                lambda x: x.product_id == rec)
+            value = sum([ProductUom._compute_quantity(
+                line.product_qty,
+                line.product_uom,
+                rec.uom_po_id) for line in lines])
+            rec.update({'qty_purchase': value})
+
+    @api.multi
+    def _inverse_qty_purchase(self):
+        purchase_order_id = self._context.get('active_id', False)
+        if not purchase_order_id:
+            return
+        purchase_order = self.env['purchase.order'].browse(purchase_order_id)
+        purchase_order_lines = purchase_order.order_line
+        for rec in self:
+            qty = rec.qty_purchase
+            lines = purchase_order_lines.filtered(
+                lambda x: x.product_id == rec)
+            if lines:
+                (lines - lines[0]).unlink()
+                lines[0].product_qty = qty
+                lines[0]._onchange_quantity()
+            else:
+                purchase_order.add_products(rec, qty)
+
+    @api.multi
+    def action_product_form(self):
+        self.ensure_one()
+        view_id = self.env['ir.model.data'].xmlid_to_res_id(
+            'product.product_normal_form_view')
+        return {
+            'name': _('Product'),
+            'view_type': 'form',
+            "view_mode": 'form',
+            'res_model': 'product.product',
+            'type': 'ir.actions.act_window',
+            # 'domain': [('id', 'in', self.apps_product_ids.ids)],
+            'res_id': self.id,
+            'view_id': view_id,
+        }
