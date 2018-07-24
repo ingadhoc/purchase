84a0c2fdb842ae8f575e0f6fbc33730ef069070f
nicomacr <nmr@adhoc.com.ar>
2018-07-24 11:57:06 -0300

    [REM] remove purchase_usability_return_invoicing, becase now implement in purchase_ux

--- purchase_usability_return_invoicing/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import purchase_order
-from . import purchase_order_line
-from . import account_invoice
