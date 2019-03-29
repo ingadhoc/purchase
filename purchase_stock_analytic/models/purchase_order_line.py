0b33efb478fede54123ae061c3e7898053329ea9
Valentino <vd@adhoc.com.ar>
2019-03-29 08:50:57 -0300

    [ADD] It adds functionality to carry Analytic Tags to stock moves.

---
+++ purchase_stock_analytic/models/purchase_order_line.py
@@ -0,0 +1,15 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def _prepare_stock_moves(self, picking):
+        res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
+        for rec in res:
+            rec['analytic_tag_ids'] = [(6, 0, self.analytic_tag_ids.ids)]
+        return res
