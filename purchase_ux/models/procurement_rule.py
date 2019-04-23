8f9797b287977551cb1114f32a4500d66c9f8014
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-04-23 16:47:21 -0300

    [ADD] purchase_ux: add the anlog logic to update the  price unit when run the stock planner an exist a purchase order in draft (#58)

--- purchase_ux/models/procurement_rule.py
+++ purchase_ux/models/procurement_rule.py
@@ -33,3 +33,25 @@ class ProcurementRule(models.Model):
                     price_unit, to_uom_id=res['product_uom'])
             res['price_unit'] = price_unit
         return res
+
+    def _update_purchase_order_line(
+            self, product_id, product_qty, product_uom, values, line, partner):
+        res = super(ProcurementRule, self)._update_purchase_order_line(
+            product_id, product_qty, product_uom, values, line, partner)
+
+        # if price was not computed (not seller or seller price = 0.0), then
+        # use standar price
+        if not res['price_unit']:
+            price_unit = product_id.with_context(
+                force_company=values['company_id'].id).standard_price
+            company_currency = values['company_id'].currency_id
+            if (price_unit and line.order_id.currency_id != company_currency):
+                price_unit = company_currency.compute(
+                    price_unit, line.order_id.currency_id)
+            if (
+                    price_unit and product_uom and
+                    product_id.uom_id != product_uom):
+                price_unit = product_id.uom_id._compute_price(
+                    price_unit, to_uom_id=product_uom.id)
+            res['price_unit'] = price_unit
+        return res
