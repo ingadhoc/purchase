49a0070789b64af0acdd9806c73d7be514185c19
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-06-18 16:06:56 -0300

    [IMP] purchase_ux: Improvements to Purchase orders and Currency
    Update prices before apply the change currency to an Purchase order

--- purchase_ux/wizards/purchase_change_currency.py
+++ purchase_ux/wizards/purchase_change_currency.py
@@ -51,6 +51,8 @@ class PurchaseChangeCurrency(models.TransientModel):
         self.ensure_one()
         purchase_order = self.get_purchase()
         for line in purchase_order.order_line:
+            line.price_unit = False
+            line._onchange_quantity()
             line.price_unit = self.currency_id.round(
                 line.price_unit * self.currency_rate)
         purchase_order.currency_id = self.currency_id.id
