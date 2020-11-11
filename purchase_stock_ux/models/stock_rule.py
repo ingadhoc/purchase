380774b0c6fa3ac3c1f60d035768832054497a44
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-11-11 10:29:44 -0300

    [13.0] [FIX] purchase_stock_ux: Fix the args of the method and the type of them.

--- purchase_stock_ux/models/stock_rule.py
+++ purchase_stock_ux/models/stock_rule.py
@@ -2,29 +2,29 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class StockRule(models.Model):
     _inherit = 'stock.rule'
 
+    @api.model
     def _prepare_purchase_order_line(
-            self, product_id, product_qty, product_uom, values, po, partner):
-        self.ensure_one()
+            self, product_id, product_qty, product_uom, company_id, values, po):
         res = super()._prepare_purchase_order_line(
             product_id=product_id, product_qty=product_qty,
-            product_uom=product_uom, values=values,
-            po=po, partner=partner)
+            product_uom=product_uom, company_id=company_id, values=values,
+            po=po)
         # if price was not computed (not seller or seller price = 0.0), then
         # use standar price
         if not res['price_unit']:
             price_unit = product_id.with_context(
-                force_company=values['company_id'].id).standard_price
-            company_currency = values['company_id'].currency_id
+                force_company=company_id.id).standard_price
+            company_currency = company_id.currency_id
             if (
                     price_unit and po.currency_id != company_currency):
                 price_unit = company_currency._convert(
-                    price_unit, po.currency_id, values['company_id'],
+                    price_unit, po.currency_id, company_id,
                     po.date_order or fields.Date.today())
             if (
                     price_unit and res['product_uom'] and
@@ -35,20 +35,19 @@ class StockRule(models.Model):
         return res
 
     def _update_purchase_order_line(
-            self, product_id, product_qty, product_uom, values, line, partner):
-        res = super()._update_purchase_order_line(
-            product_id, product_qty, product_uom, values, line, partner)
+            self, product_id, product_qty, product_uom, company_id, values, line):
+        res = super()._update_purchase_order_line(product_id, product_qty, product_uom, company_id, values, line)
 
         # if price was not computed (not seller or seller price = 0.0), then
         # use standar price
         if not res['price_unit']:
             price_unit = product_id.with_context(
-                force_company=values['company_id'].id).standard_price
-            company_currency = values['company_id'].currency_id
+                force_company=company_id.id).standard_price
+            company_currency = company_id.currency_id
             if (price_unit and line.order_id.currency_id != company_currency):
                 price_unit = company_currency._convert(
                     price_unit, line.order_id.currency_id,
-                    values['company_id'],
+                    company_id,
                     line.order_id.date_order or fields.Date.today())
             if (
                     price_unit and product_uom and
