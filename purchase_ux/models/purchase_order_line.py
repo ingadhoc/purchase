90b6112664b55af32688bdcba8dbcd1807817aa4
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-01-22 17:04:20 -0300

    [13.0] [FIX] purchase_ux: force to create invoice line.

--- purchase_ux/models/purchase_order_line.py
+++ purchase_ux/models/purchase_order_line.py
@@ -229,6 +229,7 @@ class PurchaseOrderLine(models.Model):
                 new_line._onchange_mark_recompute_taxes()
                 vals = new_line._convert_to_write(new_line._cache)
                 invoice_lines = purchase_lines.create(vals)
+                invoice_lines.exclude_from_invoice_tab = False
                 invoice_lines._onchange_balance()
                 invoice_lines.mapped('move_id')._onchange_invoice_line_ids()
             if do_not_compute:
