6c4286e2c2b7186e9c61cdc38ed84bc98eca1fa4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-02-18 08:27:23 -0300

    IMP

--- purchase_global_discount/__manifest__.py
+++ purchase_global_discount/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Purchase Global Discount',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'category': 'Purchase Management',
     'sequence': 14,
     'author': 'ADHOC SA',
