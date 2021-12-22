22e6f35d23f86467f0e0e82940a782585b90c44a
Pablo Paez <pp@adhoc.com.ar>
2021-12-22 09:06:47 -0300

    [MIG] purchase_quotation_products: Migration to 15.0

--- purchase_quotation_products/__manifest__.py
+++ purchase_quotation_products/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Purchase Quotation Products',
-    'version': '13.0.1.1.0',
+    'version': "15.0.1.0.0",
     'category': 'Purchase Management',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -33,7 +33,7 @@
     'data': [
         'views/purchase_order_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
