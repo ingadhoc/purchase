de419aa2fc7c8b4240becfcd4c30a62df01db4f9
Ignacio Cainelli <ica@adhoc.com.ar>
2022-08-25 19:27:26 +0000

    [FW][IMP] purchase_ux: account_move refactor
    Created a new 'get_product_lines_to_update' method to make it easier
    to inherit from the 'update_prices_with_supplier_cost' method
    
    closes ingadhoc/purchase#147
    
    X-original-commit: 6857a56cc782cb3d34c0f0a2207d5be7fc563812
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>
    Signed-off-by: Nacho Cainelli <ica@adhoc.com.ar>

--- purchase_ux/models/account_move.py
+++ purchase_ux/models/account_move.py
@@ -66,8 +66,7 @@ class AccountMove(models.Model):
     def update_prices_with_supplier_cost(self):
         net_price_installed = 'net_price' in self.env[
             'product.supplierinfo']._fields
-        for rec in self.with_company(self.company_id.id).invoice_line_ids.filtered(
-                lambda x: x.product_id and x.price_unit):
+        for rec in self.get_product_lines_to_update():
             seller = rec.product_id._select_seller(
                 partner_id=rec.move_id.partner_id,
                 # usamos minimo de cantidad 0 porque si no seria complicado
@@ -99,3 +98,7 @@ class AccountMove(models.Model):
                 seller.price = rec.move_id.currency_id._convert(
                     price_unit, seller.currency_id, rec.move_id.company_id,
                     rec.move_id.invoice_date or fields.Date.today())
+
+    def get_product_lines_to_update(self):
+        return self.with_context(force_company=self.company_id.id).invoice_line_ids.filtered(
+                lambda x: x.product_id and x.price_unit)
