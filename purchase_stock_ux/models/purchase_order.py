c1b52af07aa970921289c2504d99e8a6264e7783
matiasperalta1 <matias_peralta1@hotmail.com>
2022-10-31 15:29:10 -0300

    [MIG] purchase_ux: Migration to 16.0

--- purchase_stock_ux/models/purchase_order.py
+++ purchase_stock_ux/models/purchase_order.py
@@ -80,13 +80,14 @@ class PurchaseOrder(models.Model):
         return super().create(vals_list)
 
     @api.model
-    def check_force_delivered_status(self, vals):
-        if vals.get('force_delivered_status') and not self.user_has_groups(
-                'base.group_system'):
-            group = self.env.ref('base.group_system').sudo()
-            raise UserError(_(
-                'Only users with "%s / %s" can Set Received manually') % (
-                group.category_id.name, group.name))
+    def check_force_delivered_status(self, vals_list):
+        if not self.user_has_groups('base.group_system'):
+            for vals in vals_list:
+                if vals.get('force_delivered_status'):
+                    group = self.env.ref('base.group_system').sudo()
+                    raise UserError(_(
+                        'Only users with "%s / %s" can Set Received manually') % (
+                        group.category_id.name, group.name))
 
     def button_cancel(self):
         self = self.with_context(cancel_from_order=True)
