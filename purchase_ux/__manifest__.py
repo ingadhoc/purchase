89a9e45fe0e316e33e41c1b907ae33bff609f40b
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-02-07 16:13:37 -0300

    [MIG] purchase_ux: Migration to 17.0

--- purchase_ux/__manifest__.py
+++ purchase_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Purchase UX',
-    'version': "16.0.1.3.0",
+    'version': "17.0.1.0.0",
     'category': 'Purchases',
     'sequence': 14,
     'summary': '',
@@ -47,7 +47,7 @@
     'demo': [
         'demo/purchase_order_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
