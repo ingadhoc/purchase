505669905d5188e97c077ea7f143e5a8b6fdbc5f
mav-adhoc <mav@adhoc.com.ar>
2024-06-04 09:37:37 -0300

    [IMP]purchase_ux: return invoices lines filtered by company
    X-original-commit: a858490c1e3cf5016e96b6e75a6e33f978b0d1bb

---
+++ purchase_ux/models/ir_model_access.py
@@ -0,0 +1,17 @@
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from odoo import api, models, exceptions, _
+class IrModelAccess(models.Model):
+    _inherit = 'ir.model.access'
+    @api.model
+    def check(self, model, mode='read', raise_exception=True):
+        if isinstance(model, models.BaseModel):
+            assert model._name == 'ir.model', 'Invalid model object'
+            model_name = model.model
+        else:
+            model_name = model
+        # we need to use this flag to know when the operation is from this modules
+        if self._context.get('sale_quotation_products') or self._context.get('purchase_quotation_products') or self.env.is_superuser():
+            return True
