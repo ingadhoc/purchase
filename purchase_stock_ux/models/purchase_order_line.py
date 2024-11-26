7c58a45419a2220226393f739e913dd11782c539
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-11-26 13:01:42 -0300

    [FIX]purchase_stock_ux: user_id in PO

--- purchase_stock_ux/models/purchase_order_line.py
+++ purchase_stock_ux/models/purchase_order_line.py
@@ -189,15 +189,6 @@ class PurchaseOrderLine(models.Model):
 
         return res
 
-    @api.model
-    def _prepare_purchase_order_line(self, product_id, product_qty, product_uom, company_id, supplier, po):
-        res = super()._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, company_id, supplier, po)
-        #copiamos user_id desde el reabastecimiento a la orden de compra
-        if not po.user_id:
-            po.user_id = self.env.user
-        return res
-
     @api.depends(
         'qty_invoiced', 'qty_received', 'order_id.state', 'qty_returned')
     def _compute_qty_invoiced(self):
