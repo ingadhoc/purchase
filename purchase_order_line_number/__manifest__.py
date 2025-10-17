3a17e3882dbbc64865cf8b4d3e20c3503c23cc8d
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-17 12:06:11 +0000

    [MIG] purchase_order_line_number: Migration to 19.0

--- purchase_order_line_number/__manifest__.py
+++ purchase_order_line_number/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Purchase Order Line Number",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Purchase",
     "sequence": 14,
     "summary": "Add line numbers to purchase orders for better control and communication",
@@ -33,7 +33,7 @@
     "data": [
         "views/purchase_order_views.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
