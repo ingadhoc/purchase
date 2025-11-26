296d4a49964fabf94ffb3b6362a47b460f9e0be7
Rocío Vega <rov@adhoc.com.ar>
2025-11-26 15:28:39 +0000

    [REF] purchase_order_type_ux: Add refactor of multicompany behavior in order to be consitent with sale_order_type_ux feature
    X-original-commit: a0f857f29d2b0bc01d1819b69aefa162020ab2a8

--- purchase_order_type_ux/__manifest__.py
+++ purchase_order_type_ux/__manifest__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Purchase Order Type UX",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Purchases",
     "sequence": 14,
     "summary": "",
@@ -28,12 +28,7 @@
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
     "images": [],
-    "depends": [
-        "project",
-        "purchase_stock_ux",
-        "l10n_ar_purchase",
-        "purchase_order_type",
-    ],
+    "depends": ["project", "purchase_stock_ux", "l10n_ar_purchase", "purchase_order_type", "account_multicompany_ux"],
     "data": [
         "views/purchase_order_type_views.xml",
         "views/account_move_views.xml",
