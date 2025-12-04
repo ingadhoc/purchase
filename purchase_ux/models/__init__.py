c78fff7fa536f1767d0db1544433303301516f95
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-12-04 19:18:21 +0000

    [IMP] Add configuration option to skip file upload in purchase order invoice creation
    Part-of: ingadhoc/purchase#290
    Signed-off-by: Matias Velazquez <mav@adhoc.com.ar>

--- purchase_ux/models/__init__.py
+++ purchase_ux/models/__init__.py
@@ -2,7 +2,10 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import account_move
-from . import purchase_order
-from . import purchase_order_line
-from . import product_template
+from . import (
+    account_move,
+    product_template,
+    purchase_order,
+    purchase_order_line,
+    res_config_settings,
+)
