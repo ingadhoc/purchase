642671dac8a0d41f0d64d559b08407295de5496e
mav-adhoc <mav@adhoc.com.ar>
2026-01-22 09:31:57 -0300

    [IMP]purchase_request_ux: modify name field un PR view

--- purchase_request_ux/__manifest__.py
+++ purchase_request_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Purchase Request UX",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Purchases",
     "sequence": 14,
     "summary": "",
