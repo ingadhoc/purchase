24ac6210deab59ad11d1b208716b55a705933dc1
Juan Ignacio Carreras <jc@adhoc.com.ar>
2026-05-12 17:41:41 +0000

    [ADD]purchase_ux: Add product template excel file and model changes

--- purchase_ux/models/product_template.py
+++ purchase_ux/models/product_template.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -14,3 +14,15 @@ class ProductTemplate(models.Model):
         related="seller_ids.partner_id",
         store=True,
     )
+
+    @api.model
+    def get_import_templates(self):
+        res = super().get_import_templates()
+        if self.env.context.get("purchase_product_template"):
+            return [
+                {
+                    "label": _("Import Template for Products"),
+                    "template": "/purchase_ux/static/xls/product_template.xlsx",
+                }
+            ]
+        return res
